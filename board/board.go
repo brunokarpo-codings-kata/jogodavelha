28cbbc2cbfd4aa275189af9ab48c10e91c668c36
Bruno Nogueira <ti.brunonogueira@gmail.com>
2022-03-28 23:32:42 -0300

    Created function to mark fields

--- board/board.go
+++ board/board.go
@@ -1,5 +1,9 @@
 package board
 
+import (
+	"errors"
+)
+
 type Board struct {
 	fields [3][3]string
 }
@@ -10,4 +14,12 @@ func (b *Board) Init() {
 		{"-", "-", "-"},
 		{"-", "-", "-"},
 	}
+}
+
+func (b *Board) Mark(x, y int, mark string) error {
+	if b.fields[x][y] != "-" {
+		return errors.New("you could not mark a field that already is marked")
+	}
+	b.fields[x][y] = mark
+	return nil
 }
--- board/board_test.go
+++ board/board_test.go
@@ -16,3 +16,37 @@ func Test_ShouldInitABoardWithAllFieldsEmpty(t *testing.T) {
 		}
 	}
 }
+
+func Test_ShouldAllowMarkAValidEmptyFieldOnTheBoard(t *testing.T) {
+	b := Board{}
+	b.Init()
+	mark := "X"
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			b.Mark(i, j, mark)
+
+			if b.fields[i][j] != mark {
+				t.Errorf(`The field [%v][%v] was not marked properly. Expected '%v', but got '%v'`, i, j, mark, b.fields[i][j])
+			}
+		}
+	}
+}
+
+func Test_ShouldNotAllowToMarkAValidFieldThatAlreadyBeMarked(t *testing.T) {
+	b := Board{}
+	b.Init()
+	mark := "X"
+	mark2 := "O"
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			b.Mark(i, j, mark) // valid marking
+
+			err := b.Mark(i, j, mark2) // invalid marking
+			if err == nil {
+				t.Errorf(`should not allow to mark the field [%v][%v] with mark [%v]. Expected error, and the field should have value '%v', but got %v`, i, j, mark2, mark, b.fields[i][j])
+			}
+		}
+	}
+}
